c7da53fd7f437a37474c7e8da5eb1f1fd48b72d0
meAltf <alataf.dce.cs1@gmail.com>
2024-06-16 22:40:56 +0530

    Imperative way lecture-1

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -5,7 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import beans.Person;
 
 import com.google.common.collect.ImmutableList;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,21 +18,35 @@ import org.junit.Test;
 
 public class Lecture1 {
 
-  /**
-   * Start from here.. functional prog with Java 8
-   */
+    /**
+     * stream - it's just an abstraction, just use it instead of thinking how it's implemented..
+     */
 
-  @Test
-  public void imperativeApproach() throws IOException {
-    List<Person> people = MockData.getPeople();
-    // 1. Find people aged less or equal 18
-    // 2. Then change implementation to find first 10 people
+    @Test
+    public void imperativeApproach() throws IOException {
+        List<Person> people = MockData.getPeople();
+        // 1. Find people aged less or equal 18
+        // 2. Then change implementation to find first 10 people
 
-  }
+        List<Person> youngPeople = new ArrayList<>();
 
-  @Test
-  public void declarativeApproachUsingStreams() throws Exception {
-    ImmutableList<Person> people = MockData.getPeople();
+        final int limit = 10;
+        int counter = 0;
+        for (Person person : people) {
+            if (person.getAge() <= 18) {
+                youngPeople.add(person);
+                counter++;
+                if (counter == limit) break;
+            }
+        }
+        for (Person young : youngPeople) {
+            System.out.println(young);
+        }
+    }
 
-  }
+    @Test
+    public void declarativeApproachUsingStreams() throws Exception {
+        ImmutableList<Person> people = MockData.getPeople();
+
+    }
 }
