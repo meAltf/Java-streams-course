27d93a18fb278bfbd3369e0678933b2db5be9744
meAltf <alataf.dce.cs1@gmail.com>
2024-06-17 19:12:41 +0530

    understanding .collect()

--- src/test/java/lectures/Lecture12.java
+++ src/test/java/lectures/Lecture12.java
@@ -1,20 +1,26 @@
 package lectures;
 
 import beans.Person;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture12 {
-  @Test
-  public void understandingCollect() throws Exception {
-    List<String> emails = MockData.getPeople()
-        .stream()
-        .map(Person::getEmail)
-        .collect(Collectors.toList());
+    @Test
+    public void understandingCollect() throws Exception {
+        List<String> emails = MockData.getPeople()
+                .stream()
+                .map(Person::getEmail)
+                .collect(
+                        () -> new ArrayList<String>(), //1-st argument
+                        (list, element) -> list.add(element), // 2nd argument
+                        (list1, list2) -> list1.addAll(list2)); // 3rd argument
+//        .collect(Collectors.toList());
 
-    emails.forEach(System.out::println);
-  }
+        emails.forEach(System.out::println);
+    }
 }
