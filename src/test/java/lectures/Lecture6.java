b618e8007ee293f60b1625223bb32696cd20bbab
meAltf <alataf.dce.cs1@gmail.com>
2024-06-17 11:34:26 +0530

    findAny()

--- src/test/java/lectures/Lecture6.java
+++ src/test/java/lectures/Lecture6.java
@@ -13,6 +13,11 @@ public class Lecture6 {
   @Test
   public void findAny() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    Integer anyInt = Arrays.stream(numbers)
+            .filter(n -> n < 10)
+            .findAny()
+            .get();
+    System.out.println(anyInt);
 
   }
 
