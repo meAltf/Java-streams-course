19b3752891248fabb8b93562e91b27344479500f
meAltf <alataf.dce.cs1@gmail.com>
2024-06-17 11:41:20 +0530

    findFirst()

--- src/test/java/lectures/Lecture6.java
+++ src/test/java/lectures/Lecture6.java
@@ -2,29 +2,47 @@ package lectures;
 
 
 import com.google.common.collect.Lists;
+
 import java.util.Arrays;
 import java.util.function.Predicate;
+
 import org.junit.Test;
 
 public class Lecture6 {
 
-  final Predicate<Integer> numbersLessThan10 = n -> n > 5 && n < 10;
+    final Predicate<Integer> numbersLessThan10 = n -> n > 5 && n < 10;
 
-  @Test
-  public void findAny() throws Exception {
-    Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    Integer anyInt = Arrays.stream(numbers)
-            .filter(n -> n < 10)
-            .findAny()
-            .get();
-    System.out.println(anyInt);
+    /**
+     * findAny() - Non-determinastic
+     *
+     * @throws Exception
+     */
+    @Test
+    public void findAny() throws Exception {
+        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Integer anyInt = Arrays.stream(numbers)
+                //.filter(n -> n < 10)
+                .filter(numbersLessThan10)
+                .findAny()
+                .get();
+        System.out.println(anyInt);
 
-  }
+    }
 
-  @Test
-  public void findFirst() throws Exception {
-    Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    /**
+     * findFirst() - always determinastic
+     *
+     * @throws Exception
+     */
+    @Test
+    public void findFirst() throws Exception {
+        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Integer findFirstValue = Arrays.stream(numbers)
+                .filter(numbersLessThan10)
+                .findFirst()
+                .get();
+        System.out.println(findFirstValue);
 
-  }
+    }
 }
 
