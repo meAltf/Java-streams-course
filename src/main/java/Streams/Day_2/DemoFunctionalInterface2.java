a89cd50cd1f84feb1fa8eae22855263d95ad6ecf
meAltf <alataf.dce.cs1@gmail.com>
2025-07-26 10:32:37 +0530

    Functional interface | code example

---
+++ src/main/java/Streams/Day_2/DemoFunctionalInterface2.java
@@ -0,0 +1,16 @@
+package Streams.Day_2;
+
+interface MathOperation {
+    int operate(int a, int b);
+}
+
+public class DemoFunctionalInterface2 {
+    public static void main(String[] args) {
+        MathOperation add = (a, b) -> a + b;
+        MathOperation multiply = (c, d) -> c * d;
+
+        System.out.println("Add operation: " + add.operate(5, 10));
+        System.out.println("Multiply operation: " + multiply.operate(9, 89));
+    }
+
+}
